79e3e69fb3cac907dd60e2b5519264a4ab564206
Natoilad <109770565+Natoilad@users.noreply.github.com>
2023-02-19 23:14:03 +0200

    task-3-done

--- src/components/friendList/FriendListItem.jsx
+++ src/components/friendList/FriendListItem.jsx
@@ -1,9 +1,14 @@
+import css from './FriendListItem.module.css';
+
 export const FriendListItem = ({ name, avatar, isOnline }) => {
   return (
-    <li class="item">
-      <span class="status">{isOnline}</span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{name}</p>
+    <li className={css.item}>
+      <span
+        className={css.status}
+        style={{ background: isOnline ? 'green' : 'red' }}
+      ></span>
+      <img className={css.avatar} src={avatar} alt="User avatar" width="50" />
+      <p className={css.name}>{name}</p>
     </li>
   );
 };
